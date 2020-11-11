59ea800502250491b60feb76e1305ffe388a0fc0
김동현 <minseok0130@gmail.com>
2020-11-11 09:28:54 +0900

    reduce etc

--- js/this_9.js
+++ js/this_9.js
@@ -28,8 +28,42 @@ var str = 'abc def';
 // Array.prototype.push.call(str, ', pushed string');
 // console.log(str);
 Array.prototype.concat.call(str, 'string');
-console.log(str);
-Array.prototype.every.call(str, function (char) {
-  return char != ' ';
-});
-console.log(str);
+console.log('>>> concat: ', str);
+
+/* The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value */
+var every = Array.prototype.every.call(str, (char) => char != ' ');
+console.log('>>> every: ', every);
+
+/* The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns a Boolean value. */
+var some = Array.prototype.some.call(str, (char) => char === ' ');
+console.log('>>> some: ', some);
+
+/* The map() method creates a new array populated with the results of calling a provided function on every element in the calling array */
+var newArr = Array.prototype.map.call(str, (char) => char + '!');
+console.log('>>> map: ', newArr);
+
+/* The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in single output value. 
+The reducer function takes four arguments:
+1. Accumulator(acc)
+2. Current Value(cur)
+3. Current Index(idx)
+4. Source Array(src)
+Your reducer function's returned value is assigned to the accumulator, whose value is remembered across each iteration throughout the array, and ultimately becomes the final single resulting value. 
+
+Syntax
+arr.reduce(callback( accumulator, currentValue[, index[, array]] ) {
+    // return result from executing something for accumulator or currentValue
+}[, initialValue]);
+*/
+console.log('>>> str: ', str);
+var newStr = Array.prototype.reduce.apply(str, [
+  (string, char, i, arr) => string + char + i + arr[i],
+  '',
+]);
+console.log('>>> reduce_apply: ', newStr);
+
+var newStr = Array.prototype.reduce.call(
+  str,
+  (string, char, i, arr) => string + char + i + arr[i]
+);
+console.log('>>> reduce_call: ', newStr);
