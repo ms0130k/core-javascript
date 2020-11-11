753cf86f1a1e03ca0a8b244ae525fe6aa9318bce
김동현 <minseok0130@gmail.com>
2020-11-11 17:07:42 +0900

    hobby

--- js/math_max.js
+++ js/math_max.js
@@ -12,3 +12,19 @@ var max = arr.reduce(function (a, b) {
 	return Math.max(a, b);
 });
 console.log(max);
+
+/*
+Spread syntax (...) allows an iterable such as array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.
+*/
+
+const test = function () {
+	console.log(arguments);
+	console.log(typeof arguments);
+	console.log('----------');
+	console.log(Array.prototype.push.apply(arguments, [9, 8, 'test']));
+	const arrArgs = Array.from(arguments);
+	arrArgs.push('test_');
+	console.log(arrArgs);
+	console.log(arguments);
+};
+test('abcd', 123);
