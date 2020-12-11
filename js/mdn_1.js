1326ed5deb701549a9f601310be91aa36baf5715
김동현 <minseok0130@gmail.com>
2020-12-11 15:54:53 +0900

    update

--- js/mdn_1.js
+++ js/mdn_1.js
@@ -1,15 +1,15 @@
 function Person() {
-  // Person() 생성자는 `this`를 자신의 인스턴스로 정의.
-  this.age = 0;
-  console.log(this.age);
+	// Person() 생성자는 `this`를 자신의 인스턴스로 정의.
+	this.age = 0;
+	console.log(this.age);
 
-  setInterval(function growUp() {
-    // 비엄격 모드에서, growUp() 함수는 `this`를
-    // 전역 객체로 정의하고, 이는 Person() 생성자에
-    // 정의된 `this`와 다름.
-    this.age++;
-    console.log(this);
-  }, 1000);
+	setInterval(function growUp() {
+		// 비엄격 모드에서, growUp() 함수는 `this`를
+		// 전역 객체로 정의하고, 이는 Person() 생성자에
+		// 정의된 `this`와 다름.
+		this.age++;
+		console.log(this);
+	}, 1000);
 }
 
 var p = new Person();
---
+++ js/test.js
@@ -0,0 +1,19 @@
+const target = document.getElemntById('target');
+target.addEventListener('onmouseover', function (e) {
+	alert();
+});
+// const num = [1, 2, 3, 4, 5, 6, 9, 8, 7, 0];
+// let phoneNum = '';
+
+// phoneNum += '(';
+// for (var i = 0; i < 3; i++) phoneNum += num[i];
+// phoneNum += ')';
+
+// for (var i = 3; i < 6; i++) phoneNum += num[i];
+// phoneNum += '-';
+
+// for (var i = 6; i < num.length; i++) phoneNum += num[i];
+
+// console.log(phoneNum);
+
+// console.log('------------------------');
