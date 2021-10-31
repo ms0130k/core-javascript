4410c37ebbb810e2e5a1e20c86ccf3b9cbb3f078
ms0130k <minseok0130@gmail.com>
2021-11-01 00:48:18 +0900

    update

--- js/play.js
+++ js/play.js
@@ -1,23 +1,18 @@
-'use strict';
-function execute(param1, param2) {
-  var a = 1,
-    b = 2;
-  function func() {
-    var c = 3;
-    function func2() {
-      return a + c;
-    }
-    func2();
-    console.dir(func2);
-    return a + b;
-  }
-  console.dir(func);
-  return param1 + param2 + func();
-}
-console.dir(execute);
+// 'use strict';
+var value = 'value1';
 
-execute(3, 4);
-for (let i = 0; i < 10; i++) {
-  const a = i;
-  console.log(a);
+function printValue() {
+  return value;
 }
+
+function printFunc(func) {
+  var value = 'value2';
+  function func2() {
+    return value;
+  }
+  console.dir(func2);
+  console.dir(func);
+  console.log(func());
+}
+
+printFunc(printValue);
