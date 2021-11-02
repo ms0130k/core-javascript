9daa351f227094b3783947c14900974ae94d7fa4
ms0130k <minseok0130@gmail.com>
2021-11-02 21:10:47 +0900

    update

--- js/play.js
+++ js/play.js
@@ -1,33 +1,11 @@
 'use strict';
 
-function HelloFunc(func) {
-  this.greeting = 'hello';
+function countSeconds(howMany) {
+  for (var i = 1; i <= howMany; i++) {
+    console.log(i);
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
 }
-HelloFunc.prototype.call = function (func) {
-  func ? func(this.greeting) : this.func(this.greeting);
-};
-
-const userFunc = function (greeting) {
-  console.log(greeting);
-};
-
-const hello = new HelloFunc();
-hello.func = userFunc;
-hello.call();
-
-function NewObj(obj, name) {
-  obj.func = saySomething(this, 'who', name);
-}
-
-function saySomething(obj, methodName, name) {
-  return function (greeting) {
-    obj[methodName](greeting, name);
-  };
-}
-
-NewObj.prototype.who = function (greeting, name) {
-  console.log(`${greeting} ${name}`);
-};
-
-new NewObj(hello, 'shock');
-hello.call();
+countSeconds(3);
